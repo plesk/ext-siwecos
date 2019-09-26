f7d3ee9f0d7901e9f8a2f7f1191def7c0da784a4
Viktor Vogel <viktor@plesk.com>
2019-09-26 13:06:26 +0200

    Improvement - Added check whether domain resolves to Plesk's server IP address properly

--- src/plib/library/Helper.php
+++ src/plib/library/Helper.php
@@ -42,6 +42,59 @@ class Helper
             return false;
         }
 
+        try {
+            if (!self::isResolvingToPlesk($domainObject)) {
+                return false;
+            }
+        } catch (\Exception $e) {
+            return false;
+        }
+
         return true;
+    }
+
+    /**
+     * Checks whether domain name is resolving to Plesk server directly - if not then the domain name is pointing to another
+     * IP address what happens if the user is using a proxy server or the domain is not mapping the Plesk server at all
+     *
+     * @param \pm_Domain $domainObject
+     *
+     * @return bool
+     * @throws \Exception
+     */
+    private static function isResolvingToPlesk($domainObject): bool
+    {
+        $ipResolving = false;
+
+        try {
+            $records = @dns_get_record($domainObject->getName(), DNS_A | DNS_AAAA);
+        } catch (\Exception $e) {
+
+            return $ipResolving;
+        }
+
+        if (empty($records)) {
+            return $ipResolving;
+        }
+
+        $domainIpAddresses = $domainObject->getIpAddresses();
+
+        foreach ($records as $record) {
+            $ipAddress = '';
+
+            if (isset($record['ip'])) {
+                $ipAddress = $record['ip'];
+            } elseif (isset($record['ipv6'])) {
+                $ipAddress = $record['ipv6'];
+            }
+
+            foreach ($domainIpAddresses as $domain_ip) {
+                if ($ipAddress === $domain_ip) {
+                    $ipResolving = true;
+                }
+            }
+        }
+
+        return $ipResolving;
     }
 }
